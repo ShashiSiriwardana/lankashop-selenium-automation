2525cdf0948ff2381891814a4feb3ad32b2e7dcb
ShashiSiriwardana <51070920+ShashiSiriwardana@users.noreply.github.com>
2026-03-04 23:26:21 +0530

    Initial Selenium automation framework for lankashop.ch Fix Chrome headless configuration for GitHub Actions

--- src/main/java/base/DriverFactory.java
+++ src/main/java/base/DriverFactory.java
@@ -2,11 +2,21 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
     public class DriverFactory{
         private static WebDriver driver;
 
         public static WebDriver initDriver(){
+            ChromeOptions options = new ChromeOptions();
+
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+
+
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
 
